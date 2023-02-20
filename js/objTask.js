e6ffcf4a7526913c0a1a83c004924ae878e87ddc
1945714958 <1945714958@qq.com>
2023-02-20 15:59:53 +0800

    note-sync syncing

--- js/objTask.js
+++ js/objTask.js
@@ -6,3 +6,12 @@ obj.surname = 'Smith';
 obj.name = 'Pete';
 delete obj.name;
 console.log(obj);
+
+let id = Symbol("id");
+
+let user = {
+  name: "John",
+  [id]: 123 // 而不是 "id"：123
+};
+
+console.log(user);
